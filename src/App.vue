b7cf885367ddc860ab35fce72e63b044456365c2
Erik Björn <erik.g.bjoern@gmail.com>
2021-02-15 14:09:48 +0100

    lay out row by row, easier than handling grid breakpoints

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,39 @@
 <template>
   <div>
     <div style="margin: 5px 15vw">
-      <!-- <label htmlFor="rows">Rader</label>
+      <label htmlFor="rows">Rader</label>
       <input id="rows" type="number" min="7" max="23" v-model="rows" />
-      <label htmlFor="columns">Kolumner</label>
+      <!-- <label htmlFor="columns">Kolumner</label>
       <input id="columns" type="number" min="7" step="2" v-model="columns" /> -->
-      <label htmlFor="distance">Distans</label>
+      <!-- <label htmlFor="distance">Distans</label>
       <input
         id="distance"
         type="number"
         min="1"
         :max="villageDistanceMaximum"
         v-model="villageDistance"
-      />
+      /> -->
       <button @click="reset">Reset</button>
     </div>
-    <!-- <ul class="hex-grid__list">
-      <li v-for="hex in hexesEvenRows.flat()" :key="hex.number" class="hex-grid__item">
-        <div
-          class="hex-grid__content"
-          :style="`background-color: ${hex.color}9a`"
-          :class="{
-            center: isCenterHex(hex.number),
-            hidden: isOverflowing(hex.number),
-            village: isVillageHex(hex.number),
-          }"
-        >
-          <span>{{ hex.number }}</span>
-          <PopulatedTile v-if="isCenterHex(hex.number)" type="town" />
-          <PopulatedTile v-else-if="isVillageHex(hex.number)" type="village" />
-          <ResourceTile v-else :resources="hex.resources" />
-        </div>
-      </li>
-    </ul> -->
     <div class="grid-container">
-      <div class="odd-rows-grid">
-        <div v-for="hex in hexesEvenRows.flat()" :key="hex.number">
+      <div
+        class="odd-rows-grid"
+        :style="
+          `grid-template-columns: repeat(${Math.ceil(columns / 2) -
+            (columns % 2)}, 4.5vw);`
+        "
+      >
+        <div v-for="hex in hexRowsEven.flat()" :key="hex.number">
           <ResourceTile :hex="hex" />
         </div>
       </div>
-      <div class="even-rows-grid">
-        <div v-for="hex in hexesOddRows.flat()" :key="hex.number">
+      <div
+        class="even-rows-grid"
+        :style="
+          `grid-template-columns: repeat(${Math.ceil(columns / 2)}, 4.5vw);`
+        "
+      >
+        <div v-for="hex in hexRowsOdd.flat()" :key="hex.number">
           <ResourceTile :hex="hex" />
         </div>
       </div>
@@ -50,36 +43,30 @@
 
 <script>
 import { colors } from "@/helpers/colors";
-// import { isCenter, isVillage } from "@/helpers/tilePositions";
-// import PopulatedTile from "@/components/PopulatedTile.vue";
 import ResourceTile from "./components/ResourceTile.vue";
 
 export default {
   name: "App",
   components: {
     ResourceTile,
-    // PopulatedTile,
   },
   data() {
     return {
-      columns: 12, //se till att ändra i CSS-variabeln också om denna ska ändras
+      columns: 13,
       colors: colors,
-      hexesEvenRows: [],
-      hexesOddRows: [],
+      hexRowsEven: [],
+      hexRowsOdd: [],
       rows: null, //sätts i created()
-      villageDistance: null, //sätts i created()
+      // villageDistance: null, //sätts i created()
     };
   },
   computed: {
-    // rows() {
-    //   return this.rows;
+    // hexCount() {
+    //   return this.rows * this.columns - Math.ceil(this.rows / 2);
     // },
     villageDistanceMaximum() {
       return Math.ceil(this.rows / 4 - 1);
     },
-    // hexCount() {
-    //   return this.rows * this.columns;
-    // },
   },
   methods: {
     getResource() {
@@ -92,118 +79,95 @@ export default {
     randomColor() {
       return this.colors[Math.floor(Math.random() * this.colors.length)];
     },
-    // isCenterHex(hex) {
-    //   const halfHexCount = Math.ceil(this.hexCount / 2);
-    //   const { rows, columns } = this;
-    //   return isCenter({ hex, halfHexCount, rows, columns });
-    // },
-    // isVillageHex(hex) {
-    //   const halfHexCount = Math.ceil(this.hexCount / 2);
-    //   const dist = parseInt(this.villageDistance);
-    //   const { rows, columns } = this;
+    addHexRows(difference, oldRowTotal) {
+      const { hexRowsEven, hexRowsOdd } = this;
+      const columns = parseInt(this.columns);
+      const newRowTotal = oldRowTotal + difference;
+      const previouslyHighestHexNumber = 
+        Math.ceil(((oldRowTotal / 2) * (2 * columns - (columns % 2))) / 2);
+      let hexNumber = previouslyHighestHexNumber + 1;
 
-    //   return isVillage({ hex, halfHexCount, dist, rows, columns });
-    // },
-    layOutHexes(oldValue = 0) {
-      const { hexesEvenRows, hexesOddRows, rows, columns } = this;
-      const amount = rows * columns - Math.ceil(rows / 2);
-      (hexesEvenRows[0] = []), (hexesOddRows[0] = []);
+      for (let i = oldRowTotal + 1; i <= newRowTotal; i++) {
+        const tileRow = [];
+        const tileCount = Math.ceil(columns / 2) - (i % 2);
 
-      const breakPoints = [];
-
-      for (let i = 1; i <= rows; i++) {
-        breakPoints.push(
-          i % 2 == 1
-            ? columns - 1 + Math.floor(i / 2) * (2 * columns - 1)
-            : Math.floor(i / 2) * (2 * columns - 1)
-        );
-      }
-      let breakPointCounter = 0;
-      const rowCounter = () => Math.floor(breakPointCounter / 2);
-
-      for (let hex = 1; hex <= amount; hex++) {
-        debugger;
-        const tile = {
-          number: oldValue + hex,
-          color: this.randomColor(),
-          resources: {
-            stone: this.getResource(),
-            wood: this.getResource(),
-            wheat: this.getResource(),
-          },
-        };
-
-        if (hex > breakPoints[breakPointCounter]) {
-          breakPointCounter % 2 == 0
-            ? hexesEvenRows.push([])
-            : hexesOddRows.push([]);
-
-          breakPointCounter++;
+        for (let t = 0; t < tileCount; t++, hexNumber++) {
+          const tile = {
+            number: hexNumber,
+            color: this.randomColor(),
+            resources: {
+              stone: this.getResource(),
+              wood: this.getResource(),
+              wheat: this.getResource(),
+            },
+          };
+          tileRow.push(tile);
         }
 
-        breakPointCounter % 2 == 0
-          ? hexesEvenRows[rowCounter()].push(tile)
-          : hexesOddRows[rowCounter()].push(tile);
+        i % 2 == 0
+          ? (hexRowsOdd[Math.floor(i / 2)] = tileRow)
+          : (hexRowsEven[Math.floor(i / 2)] = tileRow);
       }
 
-      // localStorage.setItem("hexData", JSON.stringify({ hexesEvenRows, hexesOddRows }));
+      // localStorage.setItem("hexData", JSON.stringify({ hexRowsEven, hexRowsOdd }));
     },
     reset() {
       // localStorage.removeItem("hexData");
       localStorage.removeItem("rowCount");
-      localStorage.removeItem("villageDistance");
+      // localStorage.removeItem("villageDistance");
       window.location.reload();
     },
   },
   watch: {
-    // hexCount: {
-    //   handler(newValue, oldValue) {
-    //     if (oldValue == 0) return;
+    rows: {
+      handler(newValue, oldValue) {
+        if (oldValue == 0 || oldValue == null) return;
+        oldValue = parseInt(oldValue);
 
-    //     const difference = Math.abs(newValue - oldValue);
-    //     const { hexesEvenRows, hexesOddRows } = this;
+        const difference = Math.abs(newValue - oldValue);
+        // const { hexRowsEven, hexRowsOdd } = this;
 
-    //     if (newValue > oldValue) {
-    //       this.layOutHexes(difference, oldValue);
-    //     } else {
-    //       for (let i = 1; i <= difference; i++) {
-    //         hexStash.push(hexes[oldValue - i]);
-    //         hexes.pop();
-    //       }
+        if (newValue > oldValue) {
+          this.addHexRows(difference, oldValue);
+        } // else {
+        //   for (let i = 1; i <= difference; i++) {
+        //     hexStash.push(hexes[oldValue - i]);
+        //     hexes.pop();
+        //   }
 
-    //       localStorage.setItem(
-    //         "hexData",
-    //         JSON.stringify({ hexesEvenRows, hexesOddRows })
-    //       );
-    //     }
-    //   },
-    // },
+        // localStorage.setItem(
+        //   "hexData",
+        //   JSON.stringify({ hexRowsEven, hexRowsOdd })
+        // );
+        // }
+
+      localStorage.setItem("rowCount", newValue.toString());
+      },
+    },
     villageDistance(newValue) {
       localStorage.setItem("villageDistance", newValue.toString());
     },
-    rows(newValue, oldValue) {
-      if (this.villageDistance > this.villageDistanceMaximum)
-        this.villageDistance = this.villageDistanceMaximum;
+    // rows(newValue, oldValue) {
+    //   if (this.villageDistance > this.villageDistanceMaximum)
+    //     this.villageDistance = this.villageDistanceMaximum;
 
-      if (newValue > oldValue && oldValue == 8 && this.villageDistance == 1)
-        this.villageDistance = 2;
-
-      localStorage.setItem("rowCount", newValue.toString());
-    },
+    //   if (newValue > oldValue && oldValue == 8 && this.villageDistance == 1)
+    //     this.villageDistance = 2;
+    // },
   },
   created() {
     // const savedHexData = JSON.parse(localStorage.getItem("hexData"));
-    const savedVillageDistance = localStorage.getItem("villageDistance");
+    // const savedVillageDistance = localStorage.getItem("villageDistance");
     const savedRowCount = parseInt(localStorage.getItem("rowCount"));
 
-    this.villageDistance = savedVillageDistance || 2;
+    // this.villageDistance = savedVillageDistance || 2;
     this.rows = savedRowCount || 17;
 
     // if (savedHexData) {
     //   this.hexes = savedHexData.hexes;
     //   this.hexStash = savedHexData.hexStash;
     // } else {
-      this.layOutHexes();
+    this.addHexRows(this.rows, 0);
     // }
   },
 };
@@ -219,13 +183,11 @@ export default {
 .even-rows-grid,
 .odd-rows-grid {
   display: grid;
-  grid-template-columns: repeat(11, 4.5vw);
   grid-auto-rows: 3vw;
   gap: 1vw;
 }
 
 .even-rows-grid {
-  grid-template-columns: repeat(12, 4.5vw);
   position: absolute;
   top: 2vw;
   left: -2.75vw;
@@ -240,20 +202,4 @@ input {
   width: 30px;
   margin: 0 5px;
 }
-
-// .center {
-//   background-color: #9e0007 !important;
-// }
-
-// .village {
-//   background-color: #af6c00 !important;
-// }
-
-// .hex-grid__list {
-//   display: grid;
-//   gap: 0.2vw 0;
-//   grid-template-columns: repeat(auto-fill, 2vw);
-//   list-style-type: none;
-//   margin: 2vh calc(10vw - 30px);
-// }
 </style>
