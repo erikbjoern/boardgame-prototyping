fdf46ca8b18cacf97cdc2deac22bd3b46cadaf62
Erik Björn <erik.g.bjoern@gmail.com>
2021-06-12 15:15:17 +0200

    tweak dynamic colors, refactor with smooth value curves instead of hard steps

--- src/helpers/getDynamicColor.js
+++ src/helpers/getDynamicColor.js
@@ -26,43 +26,62 @@ export function getInvertedHexColor(hexColor) {
 
   const average = rgbValues.reduce((a, b) => a + b, 0) / rgbValues.length
   const blueIsSingleMax = ![rgbValues[0], rgbValues[1]].includes(max)
+  const redIsSingleMax = ![rgbValues[1], rgbValues[2]].includes(max)
+  const nonMaxValues = rgbValues.filter(v => v !== max)
+  const nonMaxAverage = nonMaxValues.length ? nonMaxValues.reduce((a, b) => a + b, 0) / nonMaxValues.length : average
 
-  // threshold decides whether to regard input as a dark shade
-  let averageThreshold = max > 180 ? 70 : 110
-  blueIsSingleMax && (averageThreshold += 20)
-  const multiple = average < averageThreshold ? rgbMax / max : average / 200
+  // threshold decides whether to regard input as a light shade
+  // and depends on how high the maximum value is
+  let averageThreshold = 120 - (20 * max / 180)
+  // blue needs higher threshold, and keep returning a light shade for higher values
+  // because a strong blue is not visibly as bright as green and red
+  blueIsSingleMax && (averageThreshold += Math.max(45, nonMaxAverage / 3))
+
+  // resulting in (x = v * multiplied           0 <= x <= 255, 0 <= x <≈ 65)
+  const multiple = average < averageThreshold ? rgbMax / max : average / 1000
 
   function getInvertedColorValue(v, index) {
+    // when (v == max) && (average < averageThreshold) multiplied will always be 255
     let multiplied = v * multiple
 
     // if result will be a bright shade, increase its brightness even more
     // else, make it even darker
     if (average < averageThreshold) {
-      if (blueIsSingleMax && index == 0) {
-        multiplied += (rgbMax - multiplied) / 1.8
-      } else if (blueIsSingleMax && index == 1) {
-        multiplied += (rgbMax - multiplied) / 1.4
-      } else if (v == max) {
-        multiplied += (rgbMax - multiplied) / 3
+      // see next comment below
+      // increase red less than green
+      if (blueIsSingleMax && index !== 2) {
+        multiplied +=
+          ((rgbMax - multiplied) * (index == 0 ? 0.75 : 0.8)) *
+          ((max - rgbValues[index]) / max)
+      } else if (redIsSingleMax && index !== 0) {
+        // take 75% of the margin between multiplied and rgbMax and multiply by:
+        // difference between max and the other values divided by rgbMax,
+        // which will equal 1 when (max == rgbMax && nonMaxAverage == 0)
+        // and ≈0 when (max ≈ nonMaxAverage), resulting in a smooth curve between different values
+        multiplied += ((rgbMax - multiplied) * 0.75) * ((max - nonMaxAverage) / max)
       } else {
-        multiplied += (rgbMax - multiplied) / 4
+        multiplied += ((rgbMax - multiplied) * (index == 0 ? 0.6 : 0.8)) * ((max - nonMaxAverage) / max)
       }
     } else {
-      multiplied -= multiplied * (2 / 3)
+      if (v == max) {
+        // increase main r/g/b value slightly
+        // if multiple values == max, distribute the amount
+        multiplied += 20 / rgbValues.filter(v => v == max).length
+      }
     }
 
-    // if result will be a dark shade, increase main r/g/b value slightly
-    if (multiplied < rgbMax / 2 && v == max) {
-      multiplied += 5
-    }
 
-    return average < averageThreshold
-      ? Math.min((rgbMax - average) / 2 + multiplied / 2, rgbMax)
-      : multiplied
+    // if (average < averageThreshold) {
+    //   // if result is a bright shade, even out output
+    //   multiplied = Math.min((rgbMax + multiplied + (rgbMax - average)) / 3, rgbMax)
+    //   multiplied += multiplied == max ? 25 / rgbValues.filter(v => v == max).length : -25 * (rgbValues.length - rgbValues.filter(v => v == max).length)
+    // }
+    return multiplied
   }
 
   const adjustedValues = rgbValues.map((v, index) => getInvertedColorValue(v, index))
 
+  // if result is bright yellow, tone it down
   if (
     adjustedValues[0] / 3 > adjustedValues[2] &&
     adjustedValues[1] / 3 > adjustedValues[2]
@@ -121,9 +140,9 @@ export function getInvertedHexcolorGrayscale(hexColor) {
 
   const invertedGrayValues = rgbValues.map(v => {
     if (average > rgbMax / 2) {
-      return rgbMax / 2 - Math.min(rgbMax / 2, (rgbMax - average + 75))
+      return rgbMax / 2 - Math.min(rgbMax / 2, rgbMax - average + 75)
     } else {
-      return rgbMax / 2 + Math.min(rgbMax / 2, (average + 75))
+      return rgbMax / 2 + Math.min(rgbMax / 2, average + 75)
     }
   })
 
